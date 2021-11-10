f0ab1aa21e7605e8211849b3cdcce3bbb236db32
Cameron <cameronkwalden@gmail.com>
2021-11-10 14:51:45 -0800

    adds peopleSchema

--- getpeople/index.js
+++ getpeople/index.js
@@ -1,10 +1,28 @@
 const dynamoose = require('dynamoose');
 
 exports.handler = async (event) => {
-    // TODO implement
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!'),
-    };
-    return response;
+
+  const peopleSchema = new dynamoose.Schema({
+    'id': Number,
+    'name': String,
+    'age': Number
+  });
+
+  let data = null;
+  let status = 500;
+
+  try {
+    data = await peopleSchema.scan().exec();
+    status = 200;
+  } catch (error) {
+    data = new Error(error);
+    status = 400;
+  }
+  
+
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify('Here are some people you might know!', data),
+  };
+  return response;
 };
