63d0aa9b247957c261e78fa546f52dce6e95f9b7
Cameron <cameronkwalden@gmail.com>
2021-11-10 15:40:36 -0800

    adds id to create person, fixes grammatical error

--- createpeople/index.js
+++ createpeople/index.js
@@ -2,6 +2,8 @@ const dynamoose = require('dynamoose');
 
 exports.handler = async (event) => {
 
+  const jsonBody = JSON.parse(event.body);
+
   const peopleSchema = new dynamoose.Schema({
     'id': Number,
     'name': String,
@@ -13,8 +15,16 @@ exports.handler = async (event) => {
   let data = null;
   let status = 500;
 
- 
-  
+  try{
+    let id = Math.floor(Math.random() * 100);
+    const person = new peopleTable({id, ...jsonBody});
+
+    data = await person.save();
+  } catch (error){
+    status = 400;
+    data = new Error(error);
+  }
+
   const response = {
     statusCode: 200,
     body: JSON.stringify(data),
